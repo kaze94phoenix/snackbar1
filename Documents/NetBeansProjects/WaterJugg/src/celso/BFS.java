96420e8630873ce1ad671a594aa1fa4f71a4e9f0
kaze94phoenix <phoenix.kazekage@gmail.com>
2016-03-31 08:11:03 +0200

    Exame1

--- Documents/NetBeansProjects/WaterJugg/src/celso/BFS.java
+++ Documents/NetBeansProjects/WaterJugg/src/celso/BFS.java
@@ -27,17 +27,14 @@ public class BFS<T> {
         
     }
     
+    //Execute the bfs
     public void roll(){
         Node<T> temp = bfs;
-        //System.out.println("Parent:"+this.bfs);
         while(!queue.isEmpty()){
         System.out.println("Queue currently "+queue);    
         System.out.println("Removing first item "+queue.peek());
         visited.add(queue.remove());
-//        if(temp.getParent()==null)
-//            visited.add(temp);
         System.out.println("Visited list "+visited);
-        //Iterator<Node<T>> iter = bfs.getParent().getChildren().iterator();
         System.out.println("Father:"+temp.getParent());
         System.out.println("Element:"+temp.getData());
         System.out.println("Children:"+temp.getChildren());
@@ -46,10 +43,6 @@ public class BFS<T> {
         }
         temp=queue.peek();
         
-        
-//        while(iter.hasNext()){
-//            queue.add(iter.next());
-//        }
     }
         Iterator<Node<T>> iter = visited.iterator();
         System.out.println("All visited");
@@ -63,6 +56,7 @@ public class BFS<T> {
      return j.getCurrent()==2;
     }
     
+    //Getting the best goal from a list of goals 
     public void bestGoal(LinkedList<Node<T>> goals){
         int lvl=0;
         LinkedList<Node<T>> goal = goals;
@@ -84,20 +78,6 @@ public class BFS<T> {
     }
 
     public static void main(String args[]){
-//        Node<Integer> n3 = new Node(7);
-//        Node<Integer> n2 = new Node(3);
-//        n3.addChild(2);
-//        n3.addChild(4);
-//        n3.addChild(5);
-//        n2.addChild(n3);
-//        //List<Node<Integer>> l= n3.getChildren();
-//        //System.out.println("last "+l.get(l.size()-1).getChildren().toString());
-//        Node<Integer> n = new Node(1);
-//        n.addChild(n2);
-//       // System.out.println("Children added n:"+n.getChildren());
-//        BFS<Integer> b = new BFS<Integer>(n);
-//        b.roll();
-       
         Jug j= new Jug(4, 0, 0);
         Jug j1= new Jug(3, 0, 0);
         Manage m = new Manage(j, j1);
