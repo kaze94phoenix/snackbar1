96c51e949e7355b692c32020cb0c92cfec471601
kaze94phoenix <phoenix.kazekage@gmail.com>
2016-04-27 15:20:01 +0200

    Mudancas na interface de insercao de usuarios e itens

--- Documents/NetBeansProjects/SnackBar2/snackbar.sql
+++ Documents/NetBeansProjects/SnackBar2/snackbar.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 15-Abr-2016 às 00:48
+-- Generation Time: 27-Abr-2016 às 15:18
 -- Versão do servidor: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -34,7 +34,10 @@ CREATE TABLE IF NOT EXISTS `balconista` (
   `nuit` int(11) NOT NULL,
   `username` varchar(16) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `tipousuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tipouruario_id` (`tipousuario_id`),
+  KEY `tipouruario_id_2` (`tipousuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -49,8 +52,16 @@ CREATE TABLE IF NOT EXISTS `item` (
   `preco` double NOT NULL,
   `tipoitem_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `tipoitem_id` (`tipoitem_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `tipoitem_id` (`tipoitem_id`),
+  KEY `tipoitem_id_2` (`tipoitem_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `item`
+--
+
+INSERT INTO `item` (`id`, `nome`, `preco`, `tipoitem_id`) VALUES
+(1, 'Refresco', 70, 2);
 
 -- --------------------------------------------------------
 
@@ -64,7 +75,8 @@ CREATE TABLE IF NOT EXISTS `item_pedido` (
   `pedido_id` int(11) NOT NULL,
   `data` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `item_id` (`item_id`,`pedido_id`)
+  KEY `item_id` (`item_id`,`pedido_id`),
+  KEY `pedido_id` (`pedido_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -78,7 +90,17 @@ CREATE TABLE IF NOT EXISTS `mesa` (
   `numero` int(11) NOT NULL,
   `mesalivre` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Extraindo dados da tabela `mesa`
+--
+
+INSERT INTO `mesa` (`id`, `numero`, `mesalivre`) VALUES
+(1, 1, 0),
+(2, 2, 0),
+(3, 4, 0),
+(4, 5, 0);
 
 -- --------------------------------------------------------
 
@@ -90,9 +112,12 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `balconista_id` int(11) NOT NULL,
   `mesa_id` int(11) NOT NULL,
+  `pago` tinyint(1) NOT NULL,
   `data` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `balconista` (`balconista_id`,`mesa_id`)
+  KEY `balconista` (`balconista_id`,`mesa_id`),
+  KEY `balconista_id` (`balconista_id`),
+  KEY `mesa_id` (`mesa_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -105,7 +130,66 @@ CREATE TABLE IF NOT EXISTS `tipoitem` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `tipoitem`
+--
+
+INSERT INTO `tipoitem` (`id`, `nome`) VALUES
+(1, 'Bebida Alcoolica'),
+(2, 'Bebida Nao Alcoolica'),
+(3, 'Comida');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipousuario`
+--
+
+CREATE TABLE IF NOT EXISTS `tipousuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `designacao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `tipousuario`
+--
+
+INSERT INTO `tipousuario` (`id`, `designacao`) VALUES
+(1, 'Proprietario'),
+(2, 'Gerente');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `balconista`
+--
+ALTER TABLE `balconista`
+  ADD CONSTRAINT `balconista_ibfk_1` FOREIGN KEY (`tipousuario_id`) REFERENCES `tipousuario` (`id`);
+
+--
+-- Limitadores para a tabela `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`tipoitem_id`) REFERENCES `tipoitem` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  ADD CONSTRAINT `item_pedido_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `item_pedido_ibfk_2` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`balconista_id`) REFERENCES `balconista` (`id`),
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`mesa_id`) REFERENCES `mesa` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
